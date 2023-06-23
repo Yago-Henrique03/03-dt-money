ef8b955b41b5b2162c2ee68aae58713956826471
Yago Henrique <yagohenriquest@gmail.com>
2023-06-23 14:11:27 -0300

    react-hook-form

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,28 +1,10 @@
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
-import { TransactionsContext } from "../../contexts/Transactions";
-import { useContext } from "react";
+import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary () {
-  const { transactions } = useContext(TransactionsContext)
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if(transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-
-      return acc
-    },
-    { 
-      income: 0, 
-      outcome: 0, 
-      total: 0 
-    }
-  )
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
@@ -32,7 +14,9 @@ export function Summary () {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ {summary.income}</strong>
+        <strong>  
+          {priceFormatter.format(summary.income)}
+        </strong>
       </SummaryCard>
       <SummaryCard>
         <header>
@@ -40,7 +24,7 @@ export function Summary () {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ {summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
@@ -48,7 +32,7 @@ export function Summary () {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ {summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
